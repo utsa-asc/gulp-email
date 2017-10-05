d2416140581da79a5f3df4e0bf2fe0c17aa285ec
John David Garza <garza@cjas.org>
2017-10-05 17:34:16 -0500

    initial emailer repo init

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var gulpIf      = require('gulp-if');
+var reload      = browserSync.reload;
+var email       = require('gulp-mail');
+var smtpInfo = {
+  host: 'osmtp.utsa.edu',
+  secureConnection: false,
+  port: 25
+};
+
+gulp.task('email', function () {
+  return gulp.src('./app/index.html')
+    .pipe(email({
+      subject: 'We are UTSA! - GULP EMAIL',
+      to: [
+        'john.garza@utsa.edu'
+      ],
+      from: 'WebTeam <webteam@utsa.edu>',
+      smtp: smtpInfo
+    }));
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: './app',
+        }
+    });
+    gulp.watch(src.html, reload);
+    gulp.watch(src.js, reload);
+});
+
+gulp.task('default', ['serve']);
